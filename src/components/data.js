57afeb6d0ad1e372349e67a185f15a2d87cdb85f
solujr <saloni9968066@gmail.com>
2023-05-06 14:39:18 +0530

    Initial commit

---
+++ src/components/data.js
@@ -0,0 +1,40 @@
+export const data = [
+
+    {
+
+
+        id:1,
+        title: 'Why we use React library?',
+        desc: "There are basically 2 reasons for using react first one :React is a Single page application(SPA) that means it does not reload the page it just do some changes on virtual dom and second reason is we can reuse the code easily.",
+
+
+
+
+    },
+    {
+
+
+        id:2,
+        title: 'How many types of react components are there?',
+        desc: "There are 2 types of component 1.class based component 2.functional compoonent which is function based.",
+
+
+
+
+    },
+    {
+
+
+        id:3,
+        title: 'why we use npm?',
+        desc: "The name npm (Node Package Manager) stems from when npm first was created as a package manager for Node.js.",
+
+
+
+
+    },
+
+
+
+
+]
